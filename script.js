3859949701e5ff480fdd71d75794cb6c8ab5d2fb
Jim Smits <jim.smits@stayonline.com>
2025-05-09 01:09:51 -0400

    Add spotlight effect to hero sections and implement script for dynamic background

---
+++ script.js
@@ -0,0 +1,7 @@
+const spotlight = document.querySelector('.spotlight');
+document.querySelector('.hero').addEventListener('mousemove', e => {
+  const { left, top } = e.currentTarget.getBoundingClientRect();
+  const x = e.clientX - left;
+  const y = e.clientY - top;
+  spotlight.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255,255,255,0.1) 0%, rgba(0,0,0,0.8) 80%)`;
+});
