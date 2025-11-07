c85d759de3e1678ef0033cb46b38268c1a4be66b
Jim Smits <jim.smits@stayonline.com>
2025-11-06 23:26:17 -0500

    update styles and html

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  const spotlight = document.querySelector('.spotlight');
-  document.querySelector('.hero').addEventListener('mousemove', e => {
-    const { left, top } = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - left;
-    const y = e.clientY - top;
-    spotlight.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255,255,255,0.1) 0%, rgba(0,0,0,0.8) 80%)`;
-  });
+  // const spotlight = document.querySelector('.spotlight');
+  // document.querySelector('.hero').addEventListener('mousemove', e => {
+  //   const { left, top } = e.currentTarget.getBoundingClientRect();
+  //   const x = e.clientX - left;
+  //   const y = e.clientY - top;
+  //   spotlight.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255,255,255,0.1) 0%, rgba(0,0,0,0.8) 80%)`;
+  // });
 
   document.querySelector('.hamburger').addEventListener('click', function () {
     document.getElementById('nav').classList.toggle('open');
